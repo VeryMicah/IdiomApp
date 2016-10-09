30fe5ac94aaf0617f0a4ef8f5b8a002d498f713e
Micah O'Loughlin <micah.oloughlin@gmail.com>
2016-10-09 16:09:28 +1300

    Refined method structure Added question check method

--- app/src/main/java/micah/idiomapp/MainMenu.java
+++ app/src/main/java/micah/idiomapp/MainMenu.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainMenu extends AppCompatActivity {
+    private static Random generator = new Random(); //used to select random statement, and select random options for replacement.
+    private String origStatement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,28 +20,41 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         prepBtnNormalIdiom();
+        //// TODO: 9/10/2016 add method for prepBtnRandomIdiom()
     }
 
     public void prepBtnNormalIdiom() {
-        Resources res = getResources(); //allows access to strings.xml (& other res files)
+        //allows access to strings.xml (& other res files)
+        Resources res = getResources();
+        //get the array of statements
+        final String[] idioms = res.getStringArray(R.array.statements);
+
+        //instantiate the views we'll be using
         final Button btn_normalIdiom = (Button) findViewById(R.id.main_btn_normalIdiom);
         final TextView tv_idiom = (TextView) findViewById(R.id.main_et_idiom);
-        final String[] idioms = res.getStringArray(R.array.statements);
 
         btn_normalIdiom.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                tv_idiom.setText(getRandom(idioms));
+                origStatement = getRandom(idioms);
+                origStatement = checkIfQuestion(origStatement);
+                tv_idiom.setText(origStatement);
             }
-
-
         });
     }
 
-
     public static String getRandom(String[] array) {
-        Random generator = new Random();
+
         int randomIndex = generator.nextInt(array.length);
         return array[randomIndex];
     }
+
+    //add a question mark, if necessary //todo implement string array to scan for question openers
+    public String checkIfQuestion(String s) {
+        if (s.startsWith("Does")){
+            s = s + "?";
+        }
+        return s;
+    }
+
 }
