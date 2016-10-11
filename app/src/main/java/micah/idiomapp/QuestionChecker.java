37e7f78ac11b5508f78036d4e3dc846581955e7e
Micah O'Loughlin <micah.oloughlin@gmail.com>
2016-10-11 17:16:26 +1300

    Updated Android Studio to 2.2.1. Tidied QuestionChecker constructor.

--- app/src/main/java/micah/idiomapp/QuestionChecker.java
+++ app/src/main/java/micah/idiomapp/QuestionChecker.java
@@ -9,13 +9,19 @@ import java.util.ArrayList;
  * This class currently only checks for questions that are the entirety of their sentence.
  * Embedded questions such as "I was wondering, what is the time?" will not be detected.
  * http://web.stanford.edu/~laurik/publications/archive/questions.pdf
+ *
+ * I'm using sub-methods a lot to allow easy expansion in the future.
  */
 
 public class QuestionChecker {
-    private boolean isQuestion = false;
+    private boolean isThisAQuestion = false;
     private ArrayList<String> indicators = new ArrayList<>();
 
     public QuestionChecker(){
+        buildIndicatorList();
+    }
+
+    private void buildIndicatorList(){
         indicators.add("Can");
         indicators.add("Could");
         indicators.add("Do");
@@ -32,15 +38,6 @@ public class QuestionChecker {
         indicators.add("Would");
     }
 
-    public boolean isQuestion(String statement) {
-        //I'm using sub-methods to allow easy expansion in the future.
-
-        String word = getFirstWord(statement);
-        if (indicators.contains(word)) isQuestion = true; //Note that .contains() checks using .equals()
-
-        return isQuestion;
-    }
-
     private String getFirstWord(String sentence){
         //todo remove any sentence puctuation
 //        sentence = sentence.replaceAll("\\p{P}", "");
@@ -48,7 +45,14 @@ public class QuestionChecker {
 
         //grab the first word
         int i = sentence.indexOf(' ');
-        String word = sentence.substring(0, i);
-        return word;
+        return sentence.substring(0, i);
+    }
+
+    public boolean isQuestion(String statement) {
+
+        String word = getFirstWord(statement);
+        if (indicators.contains(word)) isThisAQuestion = true; //Note that .contains() checks using .equals()
+
+        return isThisAQuestion;
     }
 }
